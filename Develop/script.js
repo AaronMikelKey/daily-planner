405427efb11a26e02e4d874fa7ee436f09a91bba
Aaron Key <AaronMikelKey@gmail.com>
2022-07-12 18:47:05 -0400

    added save to localStorage functionality using jQuery
    Added click for save button, added function to display saved data in time blocks

--- Develop/script.js
+++ Develop/script.js
@@ -3,10 +3,8 @@ let today = new Date()
 let weekday = today.getDay()
 let month = today.getMonth()
 let year = today.getFullYear()
-let currentTime = today.toLocaleString('en-US', { hour: 'numeric', hour12: true })
-console.log(currentTime)
+let currentTime = Number(today.toLocaleString('en-US', { hour: '2-digit', hour12: false }).slice(0, 2).trim())
 const container = document.getElementsByClassName('container')
-const saveButtonArr = document.getElementsByClassName('saveBtn')
 
 // Convert day number to string
 const days = [
@@ -36,16 +34,40 @@ const months = [
 let documentDate = document.getElementById('currentDay')
 documentDate.textContent = days[weekday] + ', ' + months[month] + ' ' + today.getDate() + ' ' + year
 
-// Row color
+// Get localstorage values if present
+let savedData = JSON.parse(localStorage.getItem('savedData'))
+if (!savedData) {
+	savedData = {}
+}
+console.log(savedData)
 
+// Row color
 const timeNodes = document.getElementsByClassName('hour');
 let timeArr = Object.values(timeNodes);
 
 timeArr.forEach(time => {
-	let timeNumber = time.textContent.toLocaleString('en-US', { hour: 'numeric', hour12: true })
-	console.log(timeNumber < currentTime)
-	timeNumber < currentTime ? time.nextElementSibling.classList.add('future') : null
+	// Check for localStorage saved items
+	if (savedData[time.textContent]) {
+		console.log(savedData[time.textContent])
+		$('.hour').each(function(item) {
+			if ($(this).text() == time.textContent) {
+				$(this).siblings('textarea').val(savedData[time.textContent])
+			}
+		})
+	}
+	//Convert time text to number for comparison
+	let timeNumber = Number(time.textContent.toLocaleString('en-US', { hour: '2-digit', hour12: true }).slice(0,2).trim())
+	if (timeNumber < 6) {timeNumber += 12} //add 12 to PM numbers
+	timeNumber > currentTime ? time.nextElementSibling.classList.add('future') : null
 	timeNumber == currentTime ? time.nextElementSibling.classList.add('present') : null
-	timeNumber > currentTime ? time.nextElementSibling.classList.add('past') : null
+	timeNumber < currentTime ? time.nextElementSibling.classList.add('past') : null
 })
 
+// Get time block and textarea value, save to localstorage
+$('.saveBtn').click(function (e) { 
+	let time = $(this).siblings('.hour').text()
+	let data = $(this).siblings('textarea').val()
+	console.log(time, data)
+	savedData[time] = data
+	localStorage.setItem('savedData', JSON.stringify(savedData))
+});
